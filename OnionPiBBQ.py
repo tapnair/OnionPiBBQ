e828459b4e7016ad423e7585b08909a3671e1c70
Patrick Rainsberry <prrainsberry@yahoo.com>
2017-03-06 09:36:50 -0800

    Added submodule

--- OnionPiBBQ.py
+++ OnionPiBBQ.py
@@ -1,2 +1,37 @@
 __author__ = 'rainsbp'
 
+# from max31855.max6675 import MAX6675, MAX6675Error
+import requests
+
+
+# cs_pin = 24
+# clock_pin = 23
+# data_pin = 22
+# units = "c"
+# thermocouple = MAX6675(cs_pin, clock_pin, data_pin, units)
+# print(thermocouple.get())
+# thermocouple.cleanup()
+
+
+import requests
+import json
+
+url = "https://api.onion.io/v1/devices/4e1ad09c-45ec-440b-b3d6-59e811d7bcba/i2c_exp/oled-exp"
+
+payload = json.dumps({
+    "command": "set",
+    "params": {
+        "write": "Hello World",
+        "scroll": "right"
+    },
+    "option": "i"
+}
+)
+headers = {
+    'x-api-key': "LENEg6UrepbJIAgd8ksx9HrX7mbhy1kYNAfGZsPpugcBUfaE1hqtRccIUnruFeKN",
+    'content-type': "application/json",
+}
+
+response = requests.request("POST", url, data=payload, headers=headers)
+
+print(response.text)
