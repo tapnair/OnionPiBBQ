6cd2cd56f698c9ca0d712fe69371a92172a95dcd
Patrick Rainsberry <prrainsberry@yahoo.com>
2017-03-06 10:47:36 -0800

    uncomment

--- OnionPiBBQ.py
+++ OnionPiBBQ.py
@@ -1,27 +1,24 @@
 __author__ = 'rainsbp'
 
-# from max31855.max6675 import MAX6675, MAX6675Error
-import requests
-
-
-# cs_pin = 24
-# clock_pin = 23
-# data_pin = 22
-# units = "c"
-# thermocouple = MAX6675(cs_pin, clock_pin, data_pin, units)
-# print(thermocouple.get())
-# thermocouple.cleanup()
-
-
+from max31855.max6675 import MAX6675, MAX6675Error
 import requests
 import json
+
+cs_pin = 24
+clock_pin = 23
+data_pin = 22
+units = "f"
+thermocouple = MAX6675(cs_pin, clock_pin, data_pin, units)
+value = thermocouple.get()
+thermocouple.cleanup()
+
 
 url = "https://api.onion.io/v1/devices/4e1ad09c-45ec-440b-b3d6-59e811d7bcba/i2c_exp/oled-exp"
 
 payload = json.dumps({
     "command": "set",
     "params": {
-        "write": "Hello World",
+        "write": value,
         "scroll": "right"
     },
     "option": "i"
