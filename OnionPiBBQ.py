29cfa231c09eb28d9405f7e6a80b4b39120bbdeb
Patrick Rainsberry <prrainsberry@yahoo.com>
2017-03-06 22:31:43 -0800

    uncomment

--- OnionPiBBQ.py
+++ OnionPiBBQ.py
@@ -4,28 +4,28 @@ import requests
 import json
 import time
 
-cs_pin = 15
-clock_pin = 18
-data_pin = 14
+cs_pin = 16
+clock_pin = 1
+data_pin = 15
 units = "c"
 value = "test"
 
-# from max6675 import MAX6675, MAX6675Error
-#
-# for i in range(0, 10, 1):
-#     thermocouple = MAX6675(cs_pin, clock_pin, data_pin, units)
-#     value = thermocouple.get()
-#     thermocouple.cleanup()
-#
-#     print(value)
-#
-#     time.sleep(5)
+from max6675 import MAX6675, MAX6675Error
 
-import spidev
-import RPiSensors.max6675 as sensor_6675
+for i in range(0, 10, 1):
+    thermocouple = MAX6675(cs_pin, clock_pin, data_pin, units)
+    value = thermocouple.get()
+    thermocouple.cleanup()
 
-sensor = sensor_6675.Max6675(0, 0)
-print sensor.temperature
+    print(value)
+
+    time.sleep(5)
+
+# import spidev
+# import RPiSensors.max6675 as sensor_6675
+#
+# sensor = sensor_6675.Max6675(0, 0)
+# print sensor.temperature
 
 url = "https://api.onion.io/v1/devices/4e1ad09c-45ec-440b-b3d6-59e811d7bcba/i2c_exp/oled-exp"
 
